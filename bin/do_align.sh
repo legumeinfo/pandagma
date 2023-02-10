46d9f46fae2c4f648d402993a6e055a655cfbfa0
Steven Cannon <scannon@iastate.edu>
2023-02-09 21:05:10 -0600

    Code and data changes to for whole-chromosome alignments of gene order

--- bin/do_align.sh
+++ bin/do_align.sh
@@ -5,13 +5,12 @@ set -o nounset
 
 INDIR=$1
 OUTDIR=$2
-
 NPROC=$3
 
 for filepath in $INDIR/*; do
   file=`basename $filepath`;
-  muscle -in $filepath -out $OUTDIR/$file -quiet &
-  #clustalw2 -INFILE=$filepath -ALIGN -OUTPUT=FASTA -OUTFILE=$OUTDIR/$file -QUIET &
+  #muscle -align $filepath -output $OUTDIR/$file &
+  muscle -super5 $filepath -output $OUTDIR/$file &
 
   # allow to execute up to $NPROC in parallel
   if [[ $(jobs -r -p | wc -l) -ge $NPROC ]]; then
@@ -19,5 +18,3 @@ for filepath in $INDIR/*; do
   fi
 done
 wait
-
-
