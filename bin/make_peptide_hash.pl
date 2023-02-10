1c6f47543bd727e8affd2c37180ec98143b2ba7b
Steven Cannon <scannon@iastate.edu>
2023-02-10 11:28:37 -0600

    Drop idea of coding groups for the motif positions. Use 18 AAs for the motif, reserving Q/N for punctuation & orientation.

--- bin/make_peptide_hash.pl
+++ bin/make_peptide_hash.pl
@@ -6,10 +6,9 @@ use Getopt::Long;
 
 my $usage = <<EOS;
 Generates a list of unique strings consisting of amino acid sequences, e.g. IVWFS, PDISE, ...
-The strings are peptide-like, with alternating nonpolar (mostly) and polar (mostly) residues.
 The intended purpose is to generate codes that will be interpreted as amino acid
 sequences by alignment programs -- for example, to encode a list of elements as peptide strings.
-Glutamine and tyrosine (Q and Y) are not used; they have been reserved to indicate orientation.
+Glutamine and asparagine (Q and N) are not used; they have been reserved to indicate orientation.
 
 Usage: make_peptide_hash.pl [options]
 
@@ -29,11 +28,7 @@ GetOptions (
 die "\n$usage\n" if ( $help );
 
 # Coding groups
-my @AA = split("", "AVLIFWPMSCE");
-my @BB = split("", "SYEDHKRTFLVG");
-my @CC = split("", "ICLAWEFMSVP");
-my @DD = split("", "EDFLHYRVKTSG");
-my @EE = split("", "IFEVCWLMPSA");
+my @ZZ = split("", "AVLIFWPMSCEYDHKRGT");
 my @FF = split("", "QN");
 
 my %seen_motif;
@@ -64,43 +59,11 @@ sub make_motif {
   my $rD = int(rand(12));
   my $rE = int(rand(11));
   #my $rF = int(rand(2));
-  my $code = join("", $AA[$rA], $BB[$rB], $CC[$rC], $DD[$rD], $EE[$rE]);
+  my $code = join("", $ZZ[$rA], $ZZ[$rB], $ZZ[$rC], $ZZ[$rD], $ZZ[$rE]);
   return $code;
 }
 
 __END__
 2023 S. Cannon
 02-09 Initial version.
-
-
-	Coding groups
-count
-1	Alanine	Ala	A	1	nonpolar
-2	Valine	Val	V	2	nonpolar
-2	Leucine	Leu	L	3	nonpolar
-1	Isoleucine	Ile	I	4	nonpolar
-2	Phenylalanine	Phe	F	5	nonpolar, aromatic
-1	Tryptophan	Trp	W	6	nonpolar, aromatic
-1	Proline	Pro	P	7	nonpolar
-1	Methionine	Met	M	8	nonpolar
-2	Serine	Ser	S	9	polar
-2	Cysteine	Cys	C	10	polar
-	Glutamic acid	Glu	E	11	acidic
-
-2	Valine	Val	V	1	nonpolar
-2	Leucine	Leu	L	2	nonpolar
-2	Serine	Ser	S	3	polar
-1	Tyrosine	Tyr	Y	4	polar, aromatic
-2	Glutamic acid	Glu	E	5	acidic
-1	Aspartic acid	Asp	D	6	acidic
-1	Histidine	His	H	7	basic
-1	Lysine	Lys	K	8	basic
-1	Arginine	Arg	R	9	basic
-1	Glycine	Gln	Q	10	polar
-1	Threonine	Thr	T	11	polar
-2	Cysteine	Cys	C	12	polar
-2	Phenylalanine	Phe	F	13	nonpolar, aromatic
-
-1	Glutamine	Gln	Q	1	polar
-1	Asparagine	Asn	N	2	polar
-
+02-10 Drop idea of coding groups for the motif positions. Use 18 AAs for the motif, reserving Q/N for punctuation & orientation.
