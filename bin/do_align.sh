839af6bb24fb3469f55f0fb392355325e556ca12
Nathan Weeks <1800812+nathanweeks@users.noreply.github.com>
2023-02-18 09:21:20 -0600

    Run famsa processes sequentially to avoid processor oversubscription

--- bin/do_align.sh
+++ bin/do_align.sh
@@ -10,11 +10,6 @@ NPROC=$3
 for filepath in $INDIR/*; do
   file=`basename $filepath`;
   echo "  Computing alignment, using program famsa, for file $file"
-  famsa -t $NPROC $INDIR/$file $OUTDIR/$file &
-
-  # allow to execute up to $NPROC in parallel
-  if [[ $(jobs -r -p | wc -l) -ge $NPROC ]]; then
-    wait -n # waits for the next job to terminate
-  fi
+  famsa -t $NPROC $INDIR/$file $OUTDIR/$file
 done
 wait
