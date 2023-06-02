7f1d1957208241bdc6147965a081ff4b7532bbcd
Steven Cannon <scannon@iastate.edu>
2023-06-01 20:59:33 -0500

    Change quota input from Q & S fields to just one field - giving the expected duplication count wrt the family root.

--- bin/filter_mmseqs_by_quotas.pl
+++ bin/filter_mmseqs_by_quotas.pl
@@ -12,20 +12,20 @@ use Getopt::Long;
 use File::Basename;
 use Data::Dumper;
 
-my ($quotas, $qry_pre, $sbj_pre, $verbose, $help);
+my ($quotas, $verbose, $help);
+my $species_regex = '^([^.]+)\..+';
 
 GetOptions (
-  "quotas=s" =>    \$quotas,    # required
-  "qry_pre=s" =>   \$qry_pre,   # required
-  "sbj_pre=s" =>   \$sbj_pre,   # required
-  "verbose" =>     \$verbose,
-  "help" =>        \$help
+  "quotas=s" =>        \$quotas,    # required
+  "species_regex:s" => \$species_regex,   
+  "verbose" =>         \$verbose,
+  "help" =>            \$help
 );
 
 my $scriptname = basename($0);
 
 my $usage = <<EOS;
-  Usage: cat HOMOLOGY_FILE[S] | $scriptname -quotas FILE -qry_pre STRING -sbj_pre STRING [-options] 
+  Usage: cat HOMOLOGY_FILE[S] | $scriptname -quotas FILE [-options] 
 
   Given homology data (on STDIN) with the form:
       qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore
@@ -40,63 +40,42 @@ my $usage = <<EOS;
 
   Flags and parameters:
     -quotas   -- file with regex for filtering chromosomes in two fields, as indicated above **
-    -qry_pre   -- prefix for query string **
-    -sbj_pre   -- prefix for subject string **
+    -species_regex  -- quoted regular expression for recognizing and capturing the species ID from 
+                       the query and subject fields. Default: '^([^.]+)\..+'
     -verbose  -- (boolean) for some intermediate output to STDERR
     -help     -- (boolean) for this info
   
     ** = required
 
-  The quotas file should have three fields:
-    - field 1 matching the first part of qseqid
-    - field 2 matching the first part of sseqid
-    - field 3 being the number of expected orthologs in a gene family of sseqid relative to qseqid.
-  Note that for self-matches for a species (e.g. Glycine-Glycine), the number should be the SAME AS 
-  the number of expected gene family members for that species IF the input INCLUDES the self-matches;
-  otherwise, the number should be one less, since one of the genes is represented by the qseqid instance.
+  The quotas file should have two fields:
+    - field 1 matching the first part of the subject seqID (sseqid), sufficient to identify the 
+              species or unique taxon in gene families -- for example, "Phaseolus" or "Zea".
+    - field 2 being the number of expected paralogs in a gene family of sseqid sequences, 
+              relative to the taxonomic depth of interest. For example, at the level of the legume family, 
+              two paralogs of Phaseolus are expected relative to the family root, but four of Glycine.
 
   Example of a quotas file for four species, with self-matches included, corresponding 
-  with the evolutionary depth and WGD histories for the legume family (~70 mya):
-    Arachis	Arachis	4
-    Arachis	Cercis	1
-    Arachis	Glycine	4
-    Arachis	Phaseolus	2
-    Cercis	Arachis	4
-    Cercis	Cercis	1
-    Cercis	Glycine	4
-    Cercis	Phaseolus	2
-    Glycine Arachis 4
-    Glycine Cercis 1
-    Glycine Glycine 4
-    Glycine Phaseolus 2
-    Phaseolus Arachis 4 
-    Phaseolus Cercis 1
-    Phaseolus Glycine 4
-    Phaseolus Phaseolus 2
+  with the evolutionary depth and WGD histories for the legume family (originating ~70 mya):
+    Arachis	4
+    Cercis	1
+    Glycine	4
+    Phaseolus	2
 EOS
 
-die "\n$usage\n" 
-  if ( $help or !defined($quotas) or !defined($sbj_pre) or !defined($qry_pre) );
+die "\n$usage\n" if ( $help or !defined($quotas) );
+
+my $SP_REX = qr($species_regex);
 
 # read quotas file
 open( my $QUOTA_FH, '<', $quotas ) or die "can't open quotas $quotas: $!";
 my %quotas;
-while (<$QUOTA_FH>){ # three fields, e.g. Arachis Glycine 4
+while (<$QUOTA_FH>){ # two fields, e.g. Glycine 4
   chomp;
   next if (/^#/ || /^$/);
   my $line = $_;
-  my ($qry, $sbj, $quota) = split(/\s+/);
-  #say "AA: ($qry, $sbj, $quota)";
-  
-  if ( $qry =~ /$qry_pre/ && $sbj =~ /$sbj_pre/ ){
-    $quotas{"$qry.$sbj"} = $quota;
-    if ($verbose){ say "qry.sbj: $qry.$sbj  $quota"; }
-  }
-  
-  if ( $qry =~ /$sbj_pre/ && $sbj =~ /$qry_pre/ ){
-    $quotas{"$qry.$sbj"} = $quota;
-    if ($verbose){ say "qry.sbj: $qry.$sbj  $quota"; }
-  }
+  my ($sbj, $quota) = split(/\s+/);
+  #say "AA: ($sbj, $quota)";
+  $quotas{$sbj} = $quota;
 }
 
 # Process homology data, with default sort by query & E-value
@@ -124,27 +103,28 @@ while (my $line = <>) {
   push(@homology_by_qry, [@Sparts, @Qparts, @rest]);
 
   my $Q_sp = $Qparts[0];
-  $Q_sp =~ s/^([^.]+)\..+/$1/;
+  $Q_sp =~ s/$SP_REX/$1/;
   my $S_sp = $Sparts[0];
-  $S_sp =~ s/^([^.]+)\..+/$1/;
+  $S_sp =~ s/$SP_REX/$1/;
 
-  if ($Q_sp !~ /$qry_pre/ ){
-    die "Query prefix doesn't match -qry_pre of $qry_pre: $Q_sp\n";
-  }
-  elsif ($S_sp !~ /$sbj_pre/){
-    die "Subject prefix doesn't match -sbj_pre of $sbj_pre: $S_sp\n";
-  }
+  # say "Q_sp: $Q_sp; S_sp: $S_sp"; 
+
   my ($Qchr, $Qgn, $Qsta, $Qend, $Qor) = @Qparts;
   my ($Schr, $Sgn, $Ssta, $Send, $Sor) = @Sparts;
   my $Qgn_Schr = "$Qgn.x.$Schr";
   
+  #say "Qgn: $Qgn   Schr: $Schr   Qgn_Schr: $Qgn_Schr";
   $ct_Qgn_Schr{$Qgn}++ unless ($seen_Qgn_Schr{$Qgn_Schr});
   $seen_Qgn_Schr{$Qgn_Schr}++;
-  if ( $ct_Qgn_Schr{$Qgn} <= $quotas{"$Q_sp.$S_sp"} ){ # direction: Q ==> S b/c sorting is by Q+score and we're looking to S
+  if ( $ct_Qgn_Schr{$Qgn} <= $quotas{"$S_sp"} ){ # direction: Q ==> S b/c sorting is by Q+score and we're looking to S
     $ct1++;
-    #say "BB: $Qchr\t[$Qgn]\t[$Schr]\t$Sgn\t$ct_Qgn_Schr{$Qgn}\t$ct1";  # for debugging 
+    if ($verbose){
+      say "BB: $Qchr\t[$Qgn]\t[$Schr]\t$Sgn\t$ct_Qgn_Schr{$Qgn}\t$ct1\t$quotas{$S_sp}";  # for debugging 
+    }
     # Print Qchr Qgn Qsta Qend Sor   Schr Sgn Ssta Send Sor
-    say join("\t", @Qparts, @Sparts);
+    unless ($verbose){
+      say join("\t", @Qparts, @Sparts);
+    }
   }
 }
 
@@ -170,37 +150,29 @@ foreach my $row (@homology_by_sbj) {
 
   # Note: S and Q are swapped relative to input data, since they are swapped in @homology_by_sbj
   my $Q_sp = $parts[0];
-  $Q_sp =~ s/^([^.]+)\..+/$1/;
+  $Q_sp =~ s/$SP_REX/$1/;
   my $S_sp = $parts[5];
-  $S_sp =~ s/^([^.]+)\..+/$1/;
+  $S_sp =~ s/$SP_REX/$1/;
 
-  # Also swap qry_pre and sbj_pre
-  my $swapped_qry_pre = $sbj_pre;
-  my $swapped_sbj_pre = $qry_pre;
+  # say "Q_sp: $Q_sp; S_sp: $S_sp"; 
 
   # Now revert to Q for first group of fields and S for second group of fields
-  #say "Q_sp: $Q_sp   swapped_qry_pre: $swapped_qry_pre";
-  #say "S_sp: $S_sp   swapped_sbj_pre: $swapped_sbj_pre";
-  if ($Q_sp !~ /$swapped_qry_pre/ ){
-    die "Query prefix doesn't match -qry_pre of $swapped_qry_pre: $Q_sp\n";
-  }
-  elsif ($S_sp !~ /$swapped_sbj_pre/){
-    die "Subject prefix doesn't match -sbj_pre of $swapped_sbj_pre: $S_sp\n";
-  }
   my ($Qchr, $Qgn) = ($parts[0], $parts[1]);
   my ($Schr, $Sgn) = ($parts[5], $parts[6]);
   my $Qgn_Schr = "$Qgn.$Schr";
-  my $Qsp_Ssp = "$Q_sp.$S_sp";
   
-  #say "Quota for Q_sp and S_sp, $Qsp_Ssp: $quotas{$Qsp_Ssp}";
   #say "Qgn: $Qgn   Schr: $Schr   Qgn_Schr: $Qgn_Schr";
   $ct_Qgn_Schr{$Qgn}++ unless ($seen_Qgn_Schr{$Qgn_Schr});
   $seen_Qgn_Schr{$Qgn_Schr}++;
-  if ( $ct_Qgn_Schr{$Qgn} <= $quotas{$Qsp_Ssp}){ 
+  if ( $ct_Qgn_Schr{$Qgn} <= $quotas{$S_sp}){ 
     $ct2++;
-    #say "DD: [$Qchr]\t$Qgn\t$Schr\t[$Sgn]\t$ct_Qgn_Schr{$Qgn}\t$ct2\t$quotas{$Qsp_Ssp}";  # for debugging
+    if ($verbose){
+      say "DD: [$Qchr]\t$Qgn\t$Schr\t[$Sgn]\t$ct_Qgn_Schr{$Qgn}\t$ct2\t$quotas{$S_sp}";  # for debugging
+    }
     # Print Qchr Qgn Qsta Qor   Qend Schr Sgn Ssta Send Sor 
-    say join("\t", @parts[0..4], @parts[5..9]);
+    unless ($verbose){
+      say join("\t", @parts[0..4], @parts[5..9]);
+    }
   }
 }
 
@@ -208,4 +180,4 @@ __END__
 VERSIONS
 2023-05-16 S. Cannon. Initial version.
 2023-05-19 Add back orientation field to the output, and swap Q and S in second-pass processing
-
+2023-06-01 Change quota input from Q & S fields to just one field - giving the expected duplication count wrt the family root.
