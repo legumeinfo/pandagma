e6411e7e809d87164267a0457b8992b4181d1b89
Steven Cannon <scannon@iastate.edu>
2023-08-08 09:35:47 -0500

    Change name of kaks output directory to 05_kaksout

--- pandagma-fam.sh
+++ pandagma-fam.sh
@@ -5,7 +5,7 @@
 # Authors: Steven Cannon, Joel Berendzen, Nathan Weeks, 2020-2023
 #
 scriptname=`basename "$0"`
-version="2023-08-07"
+version="2023-08-08"
 set -o errexit -o errtrace -o nounset -o pipefail
 
 trap 'echo ${0##*/}:${LINENO} ERROR executing command: ${BASH_COMMAND}' ERR
@@ -383,7 +383,7 @@ run_mcl() {
     # https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
     # https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_06_02
     if [ -z ${ks_block_wgd_cutoff+x} ] || [ -z ${ks_pair_cutoff+x} ] || 
-        [ ! -d "${WORK_DIR}/05_kaksoutBAK" ] && [ ! "$(ls -A 05_kaksoutBAK/*.rptout)" ]; then
+        [ ! -d "${WORK_DIR}/05_kaksout" ] && [ ! "$(ls -A 05_kaksout/*.rptout)" ]; then
       echo "## One or both of ks_block_wgd_cutoff ks_pair_cutoff are unset or 05_kaksout doesn't exist; no Ks filtering will be done.";
       echo "## Combine the DAGChainer synteny pairs into a file to be clustered."
       cat 04_dag/*.aligncoords | awk '$1!~/^#/ {print $2 "\t" $6}' | awk 'NF==2' | sort -u > 05_filtered_pairs.tsv
@@ -391,7 +391,7 @@ run_mcl() {
       echo "## The ks_block_wgd_cutoff is set to '$ks_block_wgd_cutoff' and ks_pair_cutoff is set to '$ks_pair_cutoff'";
       echo "## Combine the DAGChainer synteny pairs, with additional filtering by pairwise and block Ks thresholds, into a file to be clustered."
       # Combine the synteny pairs into a file to be clustered
-      cat 05_kaksoutBAK/*.rptout | 
+      cat 05_kaksout/*.rptout | 
         awk -v PAIR_CUTOFF=$ks_pair_cutoff -v BLOCK_CUTOFF=$ks_block_wgd_cutoff '
             NF>0 && $1!~/^#/ && ($5<=PAIR_CUTOFF || $7<=BLOCK_CUTOFF) {print $1 "\t" $2}' |
           sort -u > 05_filtered_pairs.tsv
