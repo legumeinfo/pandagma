5a93a1f59ba01e96adb81176976560a760bea24d
Steven Cannon <scannon@iastate.edu>
2023-09-19 07:59:48 -0500

    Update family get_data script for new pangene sets

--- get_data/get_family.sh
+++ get_data/get_family.sh
@@ -20,11 +20,11 @@ curl -O $url_base/Aeschynomene/evenia/annotations/CIAT22838.gnm1.ann1.ZM3R/aesev
 curl -O $url_base/Aeschynomene/evenia/annotations/CIAT22838.gnm1.ann1.ZM3R/aesev.CIAT22838.gnm1.ann1.ZM3R.cds.bed.gz
 curl -O $url_base/Aeschynomene/evenia/annotations/CIAT22838.gnm1.ann1.ZM3R/aesev.CIAT22838.gnm1.ann1.ZM3R.protein_primary.faa.gz
 
-curl -O $url_base/Arachis/GENUS/pangenes/Arachis.pan1.4LN9/Arachis.pan1.4LN9.pctl25_named_protein.faa.gz
-curl -O $url_base/Arachis/GENUS/pangenes/Arachis.pan1.4LN9/Arachis.pan1.4LN9.pctl25_named_cds.fna.gz
+curl -O $url_base/Arachis/GENUS/pangenes/Arachis.pan2.5P1K/Arachis.pan2.5P1K.pctl40_named_protein.faa.gz
+curl -O $url_base/Arachis/GENUS/pangenes/Arachis.pan2.5P1K/Arachis.pan2.5P1K.pctl40_named_cds.fna.gz
 
-curl -O $url_base/Cicer/GENUS/pangenes/Cicer.pan1.SV8C/Cicer.pan1.SV8C.pctl25_named_protein.faa.gz
-curl -O $url_base/Cicer/GENUS/pangenes/Cicer.pan1.SV8C/Cicer.pan1.SV8C.pctl25_named_cds.fna.gz
+curl -O $url_base/Cicer/GENUS/pangenes/Cicer.pan2.CMWZ/Cicer.pan2.CMWZ.pctl25_named_protein.faa.gz
+curl -O $url_base/Cicer/GENUS/pangenes/Cicer.pan2.CMWZ/Cicer.pan2.CMWZ.pctl25_named_cds.fna.gz
 
 curl -O $url_base/Cercis/canadensis/annotations/ISC453364.gnm3.ann1.3N1M/cerca.ISC453364.gnm3.ann1.3N1M.protein_primary.faa.gz
 curl -O $url_base/Cercis/canadensis/annotations/ISC453364.gnm3.ann1.3N1M/cerca.ISC453364.gnm3.ann1.3N1M.cds_primary.fna.gz
@@ -34,8 +34,8 @@ curl -O $url_base/annex/Dalbergia/odorifera/annotations/SKLTGB.gnm1.ann1.R67B/da
 curl -O $url_base/annex/Dalbergia/odorifera/annotations/SKLTGB.gnm1.ann1.R67B/dalod.SKLTGB.gnm1.ann1.R67B.protein.faa.gz
 curl -O $url_base/annex/Dalbergia/odorifera/annotations/SKLTGB.gnm1.ann1.R67B/dalod.SKLTGB.gnm1.ann1.R67B.cds.bed.gz
 
-curl -O $url_base/Glycine/GENUS/pangenes/Glycine.pan3.YWTW/Glycine.pan3.YWTW.pctl25_named_protein.faa.gz
-curl -O $url_base/Glycine/GENUS/pangenes/Glycine.pan3.YWTW/Glycine.pan3.YWTW.pctl25_named_cds.fna.gz
+curl -O $url_base/Glycine/GENUS/pangenes/Glycine.pan4.RK4P/Glycine.pan4.RK4P.pctl25_named_protein.faa.gz
+curl -O $url_base/Glycine/GENUS/pangenes/Glycine.pan4.RK4P/Glycine.pan4.RK4P.pctl25_named_cds.fna.gz
 
 curl -O $url_base/Lens/culinaris/annotations/CDC_Redberry.gnm2.ann1.5FB4/lencu.CDC_Redberry.gnm2.ann1.5FB4.cds.bed.gz
 curl -O $url_base/Lens/culinaris/annotations/CDC_Redberry.gnm2.ann1.5FB4/lencu.CDC_Redberry.gnm2.ann1.5FB4.cds.fna.gz
@@ -49,11 +49,11 @@ curl -O $url_base/Lupinus/albus/annotations/Amiga.gnm1.ann1.3GKS/lupal.Amiga.gnm
 curl -O $url_base/Lupinus/albus/annotations/Amiga.gnm1.ann1.3GKS/lupal.Amiga.gnm1.ann1.3GKS.cds.fna.gz
 curl -O $url_base/Lupinus/albus/annotations/Amiga.gnm1.ann1.3GKS/lupal.Amiga.gnm1.ann1.3GKS.protein.faa.gz
 
-curl -O $url_base/Medicago/GENUS/pangenes/Medicago.pan1.XXQ6/Medicago.pan1.XXQ6.pctl25_named_protein.faa.gz
-curl -O $url_base/Medicago/GENUS/pangenes/Medicago.pan1.XXQ6/Medicago.pan1.XXQ6.pctl25_named_cds.fna.gz
+curl -O $url_base/Medicago/GENUS/pangenes/Medicago.pan2.451K/Medicago.pan2.451K.pctl25_named_protein.faa.gz
+curl -O $url_base/Medicago/GENUS/pangenes/Medicago.pan2.451K/Medicago.pan2.451K.pctl25_named_cds.fna.gz
 
-curl -O $url_base/Phaseolus/GENUS/pangenes/Phaseolus.pan1.X2PC/Phaseolus.pan1.X2PC.pctl25_named_protein.faa.gz
-curl -O $url_base/Phaseolus/GENUS/pangenes/Phaseolus.pan1.X2PC/Phaseolus.pan1.X2PC.pctl25_named_cds.fna.gz
+curl -O $url_base/Phaseolus/GENUS/pangenes/Phaseolus.pan2.G5HV/Phaseolus.pan2.G5HV.pctl25_named_protein.faa.gz
+curl -O $url_base/Phaseolus/GENUS/pangenes/Phaseolus.pan2.G5HV/Phaseolus.pan2.G5HV.pctl25_named_cds.fna.gz
 
 curl -O $url_base/Pisum/sativum/annotations/Cameor.gnm1.ann1.7SZR/pissa.Cameor.gnm1.ann1.7SZR.cds.bed.gz
 curl -O $url_base/Pisum/sativum/annotations/Cameor.gnm1.ann1.7SZR/pissa.Cameor.gnm1.ann1.7SZR.cds_primary.fna.gz
@@ -63,8 +63,8 @@ curl -O $url_base/Vicia/faba/annotations/Hedin2.gnm1.ann1.PTNK/vicfa.Hedin2.gnm1
 curl -O $url_base/Vicia/faba/annotations/Hedin2.gnm1.ann1.PTNK/vicfa.Hedin2.gnm1.ann1.PTNK.cds_primary.fna.gz
 curl -O $url_base/Vicia/faba/annotations/Hedin2.gnm1.ann1.PTNK/vicfa.Hedin2.gnm1.ann1.PTNK.protein_primary.faa.gz
 
-curl -O $url_base/Vigna/GENUS/pangenes/Vigna.pan1.X2PC/Vigna.pan1.X2PC.pctl25_named_protein.faa.gz
-curl -O $url_base/Vigna/GENUS/pangenes/Vigna.pan1.X2PC/Vigna.pan1.X2PC.pctl25_named_cds.fna.gz
+curl -O $url_base/Vigna/GENUS/pangenes/Vigna.pan2.MQQM/Vigna.pan2.MQQM.pctl25_named_protein.faa.gz
+curl -O $url_base/Vigna/GENUS/pangenes/Vigna.pan2.MQQM/Vigna.pan2.MQQM.pctl25_named_cds.fna.gz
 
 curl -O $url_base/annex/Arabidopsis/thaliana/annotations/Col0.gnm9.ann11.KH24/arath.Col0.gnm9.ann11.KH24.protein_primary.faa.gz
 curl -O $url_base/annex/Arabidopsis/thaliana/annotations/Col0.gnm9.ann11.KH24/arath.Col0.gnm9.ann11.KH24.cds_primary.fna.gz
@@ -100,11 +100,20 @@ for file in *.cds.bed.gz; do
   mv $file $base.protein.bed.gz
 done
 
+mv vicfa.Hedin2.gnm1.ann1.PTNK.cds.bed.gz vicfa.Hedin2.gnm1.ann1.PTNK.protein.bed.gz
+
 # From the pan-gene protein files, derive bed files
-for file in *_named_protein.faa.gz; do 
+for file in *.pctl25_named_protein.faa.gz; do 
   base=`basename $file .pctl25_named_protein.faa.gz`
   zcat $file | awk -v OFS="\t" '$1~/^>/ {print substr($1,2), $2, $3, substr($1,2), 0, $4}' | 
     perl -pe 's/^(\w+\.\w+\.[^_]+)_\S+/$1/' > $base.pctl25_named_protein.bed
+done
+
+# Also for Arachis, where the percentile value has been changed to 40
+for file in *.pctl40_named_protein.faa.gz; do 
+  base=`basename $file .pctl40_named_protein.faa.gz`
+  zcat $file | awk -v OFS="\t" '$1~/^>/ {print substr($1,2), $2, $3, substr($1,2), 0, $4}' | 
+    perl -pe 's/^(\w+\.\w+\.[^_]+)_\S+/$1/' > $base.pctl40_named_protein.bed
 done
 
 # Check chromosome names
