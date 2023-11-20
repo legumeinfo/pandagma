aeedf5d1b17b41d314f7b125b3b48026aafefd28
StevenCannon-USDA <steven.cannon@ars.usda.gov>
2023-11-19 19:42:30 -0600

    Make tsv the default, and print all annotations if no list is provided.

--- bin/pangene_tabularize.pl
+++ bin/pangene_tabularize.pl
@@ -10,59 +10,64 @@ use DB_File;
 use Getopt::Long;
 use feature "say";
 
-my $pan_to_gn = "pan_to_gn.db";
-my $hash_method = "bdb";
-my $to_annot = "all";
+my $pan_to_gn;
+my $hash_method = "tsv";
 my $json = 0;
-my $annot_list;
+my $list_annot;
+my $annot_str_regex='([^.]+\.[^.]+\.[^.]+\.[^.]+)\..+';
 my $help;
 
 my $usage = <<EOS;
-  Usage:  pangene_tabularize.pl -annot_list FILE [-options]
+  Usage:  pangene_tabularize.pl -pan_to_gn FILE [-options]
   
-  Given a stream of accession identifiers and a pangene file, return a table of genes in the order of the accession identifiers.
+  Given a pangene file in mcl cluster format (panID gene gene gene), return 
+  a table of genes in columns by annotation. If a list of annotations is provided, 
+  report only genes for those annotations. If no list of annotations is provided,
+  then a regular expression for extracting the annotation string will be used
+  to determine the annotation list. The default is '([^.]+\.[^.]+\.[^.]+\.[^.]+)\..+'
 
   The pangene file can either be formatted as a tabular tsv file or as as a Berkeley DB file.
   
+  If "-hash_method tsv" (default), then the pangene file should be plain-text, tab-separated:
+    panID gene1 gene2 gene3 ... 
+    panID gene1 gene2 gene3 ... 
+
   If "-hash_method bdb", then the pangene file should be structured thus, as a Berkeley DB file:
     panID 
     gene1,gene2,gene3,... 
     panID
     gene1,gene2,gene3,... 
 
-  Otherwise, if "-hash_method tsv", then the pangene file should be plain-text, tab-separated:
-    panID gene1 gene2 gene3 ... 
-    panID gene1 gene2 gene3 ... 
-
   Required:
-    -annot_list  List of annotation prefixes, e.g. 
-                   glyma.Wm82.gnm1.ann1
-                   glyma.Wm82.gnm2.ann1
-                   glyma.Wm82.gnm4.ann1
-
-  Options: 
-    -pan_to_gn   [pan_to_gn.db] Path to hash file of pangeneIDs and the list of all geneIDs in 
+    -pan_to_gn   Path to hash file of pangeneIDs and the list of all geneIDs in 
                    that pangene, either in BerkeleyDB or tsv hash format. 
-                   In either case, the gene list should be comma-separated -
-                   the panID as the key and the value as the collection of genes.
-                   For the tsv option:
-                     panID  gene1,gene2,gene3,...
+                   For the tsv option
+                     panID		gene1	gene2	gene3	...
                    For the BerkeleyDB option, the key and value alternate:
                      panID
                      gene1,gene2,gene3,...
-    -hash_method [bdb] bdb or tsv
+
+  Options: 
+    -list_annot  List of annotation prefixes. If not provided, generate the tabularized output 
+                 for the full set of accessions in the pangene file. Example:
+                   glyma.Wm82.gnm1.ann1
+                   glyma.Wm82.gnm2.ann1
+                   glyma.Wm82.gnm4.ann1
+    -annot_str_regex  A quoted regular expression for extracting the annotation string. 
+                 This will be used to identify annotations if -list_annot is not provided.
+                 Default: '([^.]+\.[^.]+\.[^.]+\.[^.]+)\..+'
+    -hash_method [tsv] bdb or tsv
     -help        This message.
 EOS
 
 GetOptions(
-  'annot_list=s'  => \$annot_list,  # required
-  'pan_to_gn:s'   => \$pan_to_gn,   # required - but default is set as pan_to_gn.db
+  'pan_to_gn=s'   => \$pan_to_gn,   
+  'list_annot:s'  => \$list_annot,  
   'hash_method:s' => \$hash_method,
   'h|help'        => \$help,
 );
 
 die "$usage\n" if $help;
-die "$usage\n" unless $annot_list;
 if ( $hash_method =~ /bdb|BerkeleyDB/i && $pan_to_gn !~ /\.db$/ ){
   die "The -hash_method was set as $hash_method but the -pan_to_gn file doesn't have a suffix of .db." .
       "Please check that the BerkeleyDB file exists. If it needs to be created, try gene_trans_db_load.sh\n";
@@ -72,7 +77,9 @@ elsif ( $hash_method =~ /tsv/ && $pan_to_gn !~ /\.tsv.gz$|\.tsv$/ ){
       "doesn't have a suffix of .tsv or .tsv.gz. If it needs to be created, try gene_trans_db_load.sh\n";
 }
 
-open my $ANN_FH, "<", $annot_list or die "Can't open in annot_list $annot_list: $!\n";
+my $ANN_REX = qr($annot_str_regex);
+
+my %seen_annots;
 
 my %pan_to_gn_hsh;
 my $P2G_FH;
@@ -91,6 +98,18 @@ elsif ($hash_method =~ /tsv/){
     chomp;
     my ($pan, @genes_in_pangene) = split("\t", $_);
 
+    unless ($list_annot){ # Identify annotations from the genes unless a list was provided
+      my $annot;
+      foreach my $gene (@genes_in_pangene){
+        $gene =~ /$ANN_REX/;
+        $annot = $1;
+        unless ($seen_annots{$annot}) {
+          $seen_annots{$annot}++;
+          #say "SEEN $annot";
+        }
+      }
+    }
+
     # Next: strip splice variant from mRNA IDs to get gene IDs
     my @genes_in_pangene_stripped = map { local $_ = $_; s/\.\D*\d+$//; $_ } @genes_in_pangene;
     my $pangene_string = join(",", @genes_in_pangene_stripped);
@@ -102,10 +121,19 @@ elsif ($hash_method =~ /tsv/){
 else { die "Flag -hash_method must be either BerkeleyDB or tsv.\n" }
 
 my @annots;
-while (<$ANN_FH>){
-  chomp;
-  my $annot = $_;
-  push @annots, $annot;
+
+if ($list_annot){ # List was provided. Put it into an array.
+  open my $ANN_FH, "<", $list_annot or die "Can't open in list_annot $list_annot: $!\n";
+  while (<$ANN_FH>){
+    chomp;
+    my $annot = $_;
+    push @annots, $annot;
+  }
+}
+else { # List wasn't provided, but it was extracted when $pan_to_gn was read.
+  foreach my $annot (sort keys %seen_annots){
+    push @annots, $annot;
+  }
 }
 print join("\t", "#pangene", @annots), "\n";
 
@@ -136,5 +164,5 @@ __END__
 
 Versions
 2023-11-01 Initial functioning version
-
+2023-11-19 Make tsv the default, and print all annotations if no list is provided.
 
