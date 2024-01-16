0976fe2417904b4d451e468b183ac53f886ec069
Nathan Weeks <1800812+nathanweeks@users.noreply.github.com>
2024-01-16 05:40:32 -0600

    Add missing get_data/Glycine_7_3_2.mk

---
+++ get_data/Glycine_7_3_2.mk
@@ -0,0 +1,39 @@
+FILES ::= \
+glyma.JD17.gnm1.ann1.CLFP.cds_primary.fna.gz \
+glyma.JD17.gnm1.ann1.CLFP.gene_models_main.bed.gz \
+glyma.JD17.gnm1.ann1.CLFP.protein_primary.faa.gz \
+glyma.Jinyuan_IGA1006.gnm1.ann1.2NNX.cds.fna.gz \
+glyma.Jinyuan_IGA1006.gnm1.ann1.2NNX.gene_models_main.bed.gz \
+glyma.Jinyuan_IGA1006.gnm1.ann1.2NNX.protein.faa.gz \
+glyma.Lee.gnm1.ann1.6NZV.cds_primary.fna.gz \
+glyma.Lee.gnm1.ann1.6NZV.gene_models_main.bed.gz \
+glyma.Lee.gnm1.ann1.6NZV.protein_primary.faa.gz \
+glyma.PI_398296.gnm1.ann1.B0XR.cds_primary.fna.gz \
+glyma.PI_398296.gnm1.ann1.B0XR.gene_models_main.bed.gz \
+glyma.PI_398296.gnm1.ann1.B0XR.protein_primary.faa.gz \
+glyma.PI_548362.gnm1.ann1.LL84.cds_primary.fna.gz \
+glyma.PI_548362.gnm1.ann1.LL84.gene_models_main.bed.gz \
+glyma.PI_548362.gnm1.ann1.LL84.protein_primary.faa.gz \
+glyma.Wm82.gnm1.ann1.DvBy.cds_primary.fna.gz \
+glyma.Wm82.gnm1.ann1.DvBy.gene_models_main.bed.gz \
+glyma.Wm82.gnm1.ann1.DvBy.protein_primary.faa.gz \
+glyma.Wm82.gnm2.ann1.RVB6.cds_primary.fna.gz \
+glyma.Wm82.gnm2.ann1.RVB6.gene_models_main.bed.gz \
+glyma.Wm82.gnm2.ann1.RVB6.protein_primary.faa.gz \
+glyma.Wm82.gnm4.ann1.T8TQ.cds_primary.fna.gz \
+glyma.Wm82.gnm4.ann1.T8TQ.gene_models_main.bed.gz \
+glyma.Wm82.gnm4.ann1.T8TQ.protein_primary.faa.gz \
+glyma.Zh13.gnm1.ann1.8VV3.cds_primary.fna.gz \
+glyma.Zh13.gnm1.ann1.8VV3.gene_models_main.bed.gz \
+glyma.Zh13.gnm1.ann1.8VV3.protein_primary.faa.gz \
+glyma.Zh35_IGA1004.gnm1.ann1.RGN6.cds.fna.gz \
+glyma.Zh35_IGA1004.gnm1.ann1.RGN6.gene_models_main.bed.gz \
+glyma.Zh35_IGA1004.gnm1.ann1.RGN6.protein.faa.gz \
+glyso.PI483463.gnm1.ann1.3Q3Q.cds_primary.fna.gz \
+glyso.PI483463.gnm1.ann1.3Q3Q.gene_models_main.bed.gz \
+glyso.PI483463.gnm1.ann1.3Q3Q.protein_primary.faa.gz \
+glyso.W05.gnm1.ann1.T47J.cds_primary.fna.gz \
+glyso.W05.gnm1.ann1.T47J.gene_models_main.bed.gz \
+glyso.W05.gnm1.ann1.T47J.protein_primary.faa.gz
+
+include $(dir $(realpath $(lastword $(MAKEFILE_LIST))))/common.mk
