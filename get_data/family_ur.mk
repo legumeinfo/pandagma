d5f69d5a962efe3c32306e58452287e2090b3a43
Steven Cannon <scannon@iastate.edu>
2024-02-03 18:11:49 -0600

    Update get_data file list for family urlegume set

---
+++ get_data/family_ur.mk
@@ -0,0 +1,37 @@
+FILES ::= \
+arath.Col0.gnm9.ann11.KH24.cds_primary.fna.gz \
+arath.Col0.gnm9.ann11.KH24.gene_models_main.bed.gz \
+arath.Col0.gnm9.ann11.KH24.protein_primary.faa.gz \
+bauva.BV-YZ2020.gnm2.ann1.RJ1G.cds_primary.fna.gz \
+bauva.BV-YZ2020.gnm2.ann1.RJ1G.gene_models_main.bed.gz \
+bauva.BV-YZ2020.gnm2.ann1.RJ1G.protein_primary.faa.gz \
+cerca.ISC453364.gnm3.ann1.3N1M.cds_primary.fna.gz \
+cerca.ISC453364.gnm3.ann1.3N1M.gene_models_main.bed.gz \
+cerca.ISC453364.gnm3.ann1.3N1M.protein_primary.faa.gz \
+chafa.ISC494698.gnm1.ann1.G7XW.cds_primary.fna.gz \
+chafa.ISC494698.gnm1.ann1.G7XW.gene_models_main.bed.gz \
+chafa.ISC494698.gnm1.ann1.G7XW.protein_primary.faa.gz \
+medtr.A17_HM341.gnm4.ann2.G3ZY.cds_primary.fna.gz \
+medtr.A17_HM341.gnm4.ann2.G3ZY.gene_models_main.bed.gz \
+medtr.A17_HM341.gnm4.ann2.G3ZY.protein_primary.faa.gz \
+phavu.G19833.gnm2.ann1.PB8d.cds_primary.fna.gz \
+phavu.G19833.gnm2.ann1.PB8d.gene_models_main.bed.gz \
+phavu.G19833.gnm2.ann1.PB8d.protein_primary.faa.gz \
+prupe.Lovell.gnm2.ann1.S2ZZ.cds_primary.fna.gz \
+prupe.Lovell.gnm2.ann1.S2ZZ.gene_models_main.bed.gz \
+prupe.Lovell.gnm2.ann1.S2ZZ.protein_primary.faa.gz \
+quisa.S10.gnm1.ann1.RQ4J.cds_primary.fna.gz \
+quisa.S10.gnm1.ann1.RQ4J.gene_models_main.bed.gz \
+quisa.S10.gnm1.ann1.RQ4J.protein_primary.faa.gz \
+sento.Myeongyun.gnm1.ann1.5WXB.cds.fna.gz \
+sento.Myeongyun.gnm1.ann1.5WXB.gene_models_main.bed.gz \
+sento.Myeongyun.gnm1.ann1.5WXB.protein.faa.gz \
+singl.CAF01.gnm1.ann1.WFKC.cds.fna.gz \
+singl.CAF01.gnm1.ann1.WFKC.gene_models_main.bed.gz \
+singl.CAF01.gnm1.ann1.WFKC.protein.faa.gz \
+vitvi.PN40024.gnm2.ann1.V31M.cds_primary.fna.gz \
+vitvi.PN40024.gnm2.ann1.V31M.gene_models_main.bed.gz \
+vitvi.PN40024.gnm2.ann1.V31M.protein_primary.faa.gz \
+legume.TE_lib_2024.rpt.6WVT.fna.gz
+
+include $(dir $(realpath $(lastword $(MAKEFILE_LIST))))/common.mk
