f4092f9f3c96876a0349cb5aa14634d498ca3bf6
Steven Cannon <scannon@iastate.edu>
2026-02-13 14:18:04 -0600

    Add config for calculating small families for conekt, using fsup

---
+++ get_data/family3_sup_conekt.mk
@@ -0,0 +1,31 @@
+FILES ::= \
+arahy.Tifrunner.gnm2.ann2.PVFB.cds.fna.gz \
+arahy.Tifrunner.gnm2.ann2.PVFB.gene_models_main.bed.gz \
+arahy.Tifrunner.gnm2.ann2.PVFB.protein.faa.gz \
+arath.Col0.gnm9.ann11.KH24.cds_primary.fna.gz \
+arath.Col0.gnm9.ann11.KH24.gene_models_main.bed.gz \
+arath.Col0.gnm9.ann11.KH24.protein_primary.faa.gz \
+cajca.ICPL87119.gnm2.ann1.L3ZH.cds_primary.fna.gz \
+cajca.ICPL87119.gnm2.ann1.L3ZH.gene_models_main.bed.gz \
+cajca.ICPL87119.gnm2.ann1.L3ZH.protein_primary.faa.gz \
+cicar.CDCFrontier.gnm3.ann1.NPD7.cds.fna.gz \
+cicar.CDCFrontier.gnm3.ann1.NPD7.gene_models_main.bed.gz \
+cicar.CDCFrontier.gnm3.ann1.NPD7.protein.faa.gz \
+glyma.Wm82.gnm4.ann1.T8TQ.cds_primary.fna.gz \
+glyma.Wm82.gnm4.ann1.T8TQ.gene_models_main.bed.gz \
+glyma.Wm82.gnm4.ann1.T8TQ.protein_primary.faa.gz \
+phavu.G19833.gnm2.ann1.PB8d.cds_primary.fna.gz \
+phavu.G19833.gnm2.ann1.PB8d.gene_models_main.bed.gz \
+phavu.G19833.gnm2.ann1.PB8d.protein_primary.faa.gz \
+prupe.Lovell.gnm2.ann1.S2ZZ.cds_primary.fna.gz \
+prupe.Lovell.gnm2.ann1.S2ZZ.gene_models_main.bed.gz \
+prupe.Lovell.gnm2.ann1.S2ZZ.protein_primary.faa.gz \
+vigun.IT97K-499-35.gnm1.ann2.FD7K.cds_primary.fna.gz \
+vigun.IT97K-499-35.gnm1.ann2.FD7K.gene_models_main.bed.gz \
+vigun.IT97K-499-35.gnm1.ann2.FD7K.protein_primary.faa.gz \
+vitvi.PN40024.gnm2.ann1.V31M.cds_primary.fna.gz \
+vitvi.PN40024.gnm2.ann1.V31M.gene_models_main.bed.gz \
+vitvi.PN40024.gnm2.ann1.V31M.protein_primary.faa.gz \
+legume.TE_lib_2024.rpt.6WVT.fna.gz
+
+include $(dir $(realpath $(lastword $(MAKEFILE_LIST))))/common.mk
